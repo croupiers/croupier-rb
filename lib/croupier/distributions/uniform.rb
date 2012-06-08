d9e0b88d0f1b1edc3b9c9b99b3911f771220fceb
Sergio Arbeo <serabe@gmail.com>
2012-06-08 21:53:03 +0200

    Corrected typos an useless whitespaces.

--- lib/croupier/distributions/uniform.rb
+++ lib/croupier/distributions/uniform.rb
@@ -1,13 +1,13 @@
 module Croupier
   module Distributions
-    
+
     #####################################################################
-    # Uniform Distribution 
-    # Continous distribution where all points in an interval have 
+    # Uniform Distribution
+    # Continous distribution where all points in an interval have
     # the same probability.
     #
     class Uniform < ::Croupier::Distribution
-      
+
       def initialize(options={})
         @name = "Uniform distribution"
         @description = "Continuous distribution on [a,b] (defaults to [0,1]) where all points in the interval are equally likely"
