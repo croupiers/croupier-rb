bf746dd9f60ec8c153f22c8a428e57e151816856
Juanjo Bazán <jjbazan@gmail.com>
2012-06-08 19:17:35 +0200

    Exponential distribution

---
+++ lib/croupier/distributions/exponential.rb
@@ -0,0 +1,39 @@
+module Croupier
+  module Distributions
+    
+    #####################################################################
+    # Uniform Distribution 
+    # Continous distribution where all points in an interval have 
+    # the same probability.
+    #
+    class Exponential < ::Croupier::Distribution
+      
+      def initialize(options={})
+        @name = "Exponential distribution"
+        @description = "Continuous probability distribution with a lambda param rate describing the time between events in a Poisson process"
+        configure(options)
+      end
+
+      def generate_single_number
+        raise Croupier::InputParamsError, "Invalid interval values" if @parameters[:lambda] <= 0
+        (-1/@parameters[:lambda]) * Math.log(rand)
+      end
+
+      def default_parameters
+        {:lambda => 1.0}
+      end
+
+      def self.cli_name
+        "exponential"
+      end
+
+      def self.cli_options
+        {:options => [
+           [:lambda, 'rate param', {:type=>:float, :default => 1.0}]
+         ],
+         :banner => "Exponential distribution. Generate numbers following a exponential distribution for a given lambda rate"
+        }
+      end
+    end
+  end
+end
