2295ec7a7cd61513ac6360fcd5a7602386435010
Sergio Arbeo <serabe@gmail.com>
2012-06-08 22:57:50 +0200

    Distributions can be declared by defining only one function.
    Previously, you needed to define generate_single_number and
    generate_numbers (if needed for performance reasons).
    
    Now, when creating a new distribution it is enough to
    define only one of them.

--- test/test_distribution_class.rb
+++ test/test_distribution_class.rb
@@ -7,7 +7,7 @@ class TestDistributionClass < MiniTest::Unit::TestCase
     assert_respond_to dist, 'name'
     assert_respond_to dist, 'description'
   end
-  
+
   def test_distribution_accepts_parameter_configuration
     dist = Croupier::Distribution.new
     assert_equal dist.parameters, dist.default_parameters
@@ -16,14 +16,16 @@ class TestDistributionClass < MiniTest::Unit::TestCase
     assert_equal dist.parameters[:larry], 'bird'
     assert_equal dist.parameters[:leonard], 'euler'
   end
-  
+
   def test_distribution_has_trollop_options_info
     assert_respond_to Croupier::Distribution, 'cli_name'
     assert_respond_to Croupier::Distribution, 'cli_options'
   end
-  
+
   def test_distribution_generates_array_of_n_results
-    assert_equal Croupier::Distribution.new.generate_numbers(15).size, 15
+    a = Croupier::Distribution.new
+    def a.generate_single_number; 15; end
+    assert_equal a.generate_numbers(15).size, 15
   end
 
 end
