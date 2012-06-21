19ade5bcaf1f7197c5ec0c56b396ffc237b90daa
Juanjo Bazán <jjbazan@gmail.com>
2012-06-21 12:25:23 +0200

    Remove warnings from R tests. And better docs on running tests.

--- RUNNING_TESTS.md
+++ RUNNING_TESTS.md
@@ -15,13 +15,27 @@ You can run these tests using the default Rake task:
 ### R tests
 
 There's a group of tests validating the probability distribution of several samples of numbers generated with Croupier.
+To generate samples to be tested, run the script ```generate_test_data.sh```:
+
+    > ./generate_test_data.sh
+
+Now you can run the R testsuite:
 These tests are statistical tests included in R scripts.
 To run them you need to have R installed in your system. You can get the R software from ```http://www.r-project.org/```
+
+Dependencies:
+
 The testsuite uses the 'testthat' package. You can install it from the R console with the folowing command:
 
     install.packages("testthat", repos = "http://cran.r-project.org/", type="source")
 
-Once you have R and all the dependencies installed in your system, you can run the test/testsuite.R file:
+Other R packages needed:
+
+    * triangle
+    * vcd
+
+The test script will try to intall all dependencies if they are not found in your R installation.
+You can run the test/testsuite.R file:
 
 Using Rscript:
 
--- test/testsuite.R
+++ test/testsuite.R
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
 #
 # This script runs all the R tests of the croupier gem for all the distributions.
+# 
 # Running the script:
 #  Option 1, Via Rscript:
 #    $> Rscript testsuite.R
@@ -8,10 +9,15 @@
 #  Option 2, from the R console:
 #    source('testsuite.R')
 #
-# In order to run the tests you need to have installed the testthat package
-# If it is not included in your R installation you can do so running this command:
-# install.packages("testthat", repos = "http://cran.r-project.org/", type="source")
-#
+# Dependencies:
+# In order to run the tests you need to have installed the following packages:
+# 
+#   - testthat
+#   - triangle
+#   - vcd
+# 
+# Croupier will try to install them automatically if they are not included
+# in your R installation.
 
 if(require('testthat') == FALSE) {
   install.packages('testthat', repos = "http://cran.r-project.org/", type="source")
@@ -26,4 +32,6 @@ if(require('vcd') == FALSE) {
   install.packages('vcd')
   require('vcd')
 }
+options(warn=-1)
 test_dir("./distributions/R_tests/")
+options(warn=0)
