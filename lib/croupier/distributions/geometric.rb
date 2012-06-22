9ed1a5859357bef6f7747e4e17a3716eef9aad5f
Juanjo Bazán <jjbazan@gmail.com>
2012-06-22 13:02:42 +0200

    Param :success for geometric distribution

--- lib/croupier/distributions/geometric.rb
+++ lib/croupier/distributions/geometric.rb
@@ -16,17 +16,17 @@ module Croupier
         @name = "Geometric distribution"
         @description = "Discrete probability distribution that expresses the the number of X Bernoulli trials needed to get one success, supported on the set { 1, 2, 3, ...}"
         configure(options)
-        raise Croupier::InputParamsError, "Param p must be in the interval [0,1]" if params[:p] > 1 || params[:p] < 0
+        raise Croupier::InputParamsError, "Probability of success must be in the interval [0,1]" if params[:success] > 1 || params[:success] < 0
       end
 
       # Fair point: it is not the inverse of the cdf,
       # but it generates the distribution from an uniform.
       def inv_cdf n
-        (Math.log(1-n) / Math.log(1-params[:p])).ceil
+        (Math.log(1-n) / Math.log(1-params[:success])).ceil
       end
 
       def default_parameters
-        {:p => 0.5}
+        {:success => 0.5}
       end
 
       def self.cli_name
@@ -35,7 +35,7 @@ module Croupier
 
       def self.cli_options
         {:options => [
-           [:p, 'success probability of each trial', {:type=>:float, :default => 0.5}]
+           [:success, 'success probability of each trial', {:type=>:float, :short => "-p", :default => 0.5}]
          ],
          :banner => "Geometric distribution. Discrete probability distribution that expresses the number of X Bernoulli trials needed to get one success, supported on the set { 1, 2, 3, ...} }"
         }
